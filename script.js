cdc07d06692f6494ad8e2df85de9a1a1c5d37f15
abdullohzakialim <zakii.lim07@gmail.com>
2026-06-22 14:10:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function pesan() {
+  alert("Terima kasih telah mengunjungi Glad2Glow!");
+}
+
+function detail(nama) {
+  alert("Produk yang dipilih adalah : " + nama);
+}
+
+function mode() {
+  document.body.classList.toggle("dark");
+}
